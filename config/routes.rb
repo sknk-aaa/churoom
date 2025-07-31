86e8bef2937483573757629827da70ad9fef5023
s-knk <625.somq2525@gmail.com>
2025-07-31 23:36:17 +0900

    曜日・時限を選択して検索結果ビューに遷移する機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get "result/time"
   get "/" => "home#top", as: :home_top
   get "about" => "home#about", as: :home_about
   get "contact" => "home#contact", as: :home_contact
+
+
 end
