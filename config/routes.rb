ad8a745f1501b47e732825f141daa7290009586c
s-knk <625.somq2525@gmail.com>
2025-08-01 16:32:26 +0900

    時間から探す検索結果ビューを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "result/time"
   get "/" => "home#top", as: :home_top
   get "about" => "home#about", as: :home_about
   get "contact" => "home#contact", as: :home_contact
+  get "result/time" => "result#time", as: :result_time
+  get "result/room" => "result#room", as: :result_room
+
 end
