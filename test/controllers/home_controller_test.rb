6cc67625df28934f80fd4bdaf8d687e7b12c9176
s-knk <625.somq2525@gmail.com>
2025-08-01 21:07:41 +0900

    lintエラーの解決

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
     get root_url
-    assert_response :redirect                 
-    assert_redirected_to home_search_path 
+    assert_response :redirect
+    assert_redirected_to home_search_path
   end
 end
