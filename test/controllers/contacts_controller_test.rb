5cac65e149e1e0d8d9afe10ffa3bc41de8ab5f80
s-knk <625.somq2525@gmail.com>
2025-09-04 15:58:40 +0900

    READMEにスクショ、画面遷移図を追加

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get contact_new_url
+    get new_contact_url
     assert_response :success
   end
 end
